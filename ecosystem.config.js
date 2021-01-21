739c9d1fd9b43f125d13ac8268e2ad502c36d693
James A. Joy <joyja@jarautomation.io>
2021-01-21 08:13:31 +0000

    changed name and github repo in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'edge-nat-deploy',
+      name: 'graphql-pm2-deploy',
       script: './src/index.js',
       env: {
         NODE_ENV: 'production',
@@ -13,8 +13,8 @@ module.exports = {
       user: 'root',
       host: 'localhost',
       ref: 'origin/main',
-      repo: 'https://github.com/joyja/edge-nat-deploy.git',
-      path: '/usr/local/bin/edge-nat-deploy',
+      repo: 'https://github.com/joyja/graphql-pm2-deploy.git',
+      path: '/usr/local/bin/graphql-pm2-deploy',
       'post-deploy':
         'sudo npm install && sudo pm2 startOrRestart ecosystem.config.js --env production',
     },
